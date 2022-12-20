e5c2fd81fdef2fb25048f32ae999dc0bc3f8271e
hmchung2 <hmchung2@gmail.com>
2022-12-21 02:37:08 +0900

    interpolate

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import { Animated, TouchableOpacity } from "react-native";
+import { Animated, Pressable, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -9,36 +9,53 @@ const Container = styled.View`
 `;
 const Box = styled.View`
   background-color: tomato;
-  width: 100px;
-  height: 100px;
+  width: 200px;
+  height: 200px;
 `;
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
   const [up, setUp] = useState(false);
-  const Y_POSITION = useRef(new Animated.Value(0)).current;
+  const Y_POSITION = useRef(new Animated.Value(300)).current;
   const toggleUp = () => setUp((prev) => !prev);
   const moveUp = () => {
     console.log("touched");
     Animated.timing(Y_POSITION, {
-      toValue: up ? 100 : -100,
+      toValue: up ? 300 : -300,
       useNativeDriver: true,
+      duration: 3000,
     }).start(toggleUp);
   };
 
-  Y_POSITION.addListener(() => console.log(Y_POSITION));
-  console.log("up : ", up);
-  console.log("y : ", Y_POSITION);
+  // console.log("up : ", up);
+  // console.log("y : ", Y_POSITION);
+
+  const opacityValue = Y_POSITION.interpolate({
+    inputRange: [-300, 0, 300],
+    outputRange: [1, 0, 1],
+  });
+
+  const borderRadius = Y_POSITION.interpolate({
+    inputRange: [-300, 300],
+    outputRange: [100, 0],
+  });
+
+  Y_POSITION.addListener(() => console.log(opacityValue));
 
   return (
     <Container>
-      <TouchableOpacity onPress={moveUp}>
+      <Pressable
+        onPress={moveUp}
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      >
         <AnimatedBox
           style={{
+            opacity: opacityValue,
+            borderRadius: borderRadius,
             transform: [{ translateY: Y_POSITION }],
           }}
         />
-      </TouchableOpacity>
+      </Pressable>
     </Container>
   );
 }
